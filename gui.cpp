cf9453708377a0a911499d7e770e11655f8540c4
ed <edtickle@icloud.com>
2025-08-13 09:42:10 +0100

    Added Download image button

--- gui.cpp
+++ gui.cpp
@@ -28,6 +28,8 @@
 
 #include "ImageManipulator.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
 // Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
@@ -329,7 +331,10 @@ int main(int, char**)
                 renderMode = 6;
             }
             
-            
+            if(ImGui::Button("Download (Found in OutputFiles)")){
+                
+                stbi_write_png("OutputFiles/image.jpg",image_details.width,image_details.height,image_details.numColourChannels,image_details.image,image_details.width * image_details.numColourChannels);
+            }
             
             ImGui::End();
         	
